a0da79c910cfed99599048091cf48c962176e253
hoang.hung <hoangvanhung02@vccorp.vn>
2020-11-17 09:06:07 +0700

    fix design home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,15 +15,18 @@
     .panel-shadow {
         box-shadow: rgba(0, 0, 0, 0.3) 7px 7px 7px;
     }
+
     .panel-white {
         border: 1px solid #dddddd;
     }
-    .panel-white  .panel-heading {
+
+    .panel-white .panel-heading {
         color: #333;
         background-color: #fff;
         border-color: #ddd;
     }
-    .panel-white  .panel-footer {
+
+    .panel-white .panel-footer {
         background-color: #fff;
         border-color: #ddd;
     }
@@ -32,70 +35,88 @@
         height: 95px;
         padding: 20px 15px;
     }
+
     .post .post-heading .avatar {
         width: 60px;
         height: 60px;
         display: block;
         margin-right: 15px;
     }
+
     .post .post-heading .meta .title {
         margin-bottom: 0;
     }
+
     .post .post-heading .meta .title a {
         color: black;
     }
+
     .post .post-heading .meta .title a:hover {
         color: #aaaaaa;
     }
+
     .post .post-heading .meta .time {
         margin-top: 8px;
         color: #999;
     }
+
     .post .post-image .image {
         width: 100%;
         height: auto;
     }
+
     .post .post-description {
         padding: 15px;
     }
+
     .post .post-description p {
         font-size: 14px;
     }
+
     .post .post-description .stats {
         margin-top: 20px;
     }
+
     .post .post-description .stats .stat-item {
         display: inline-block;
         margin-right: 15px;
     }
+
     .post .post-description .stats .stat-item .icon {
         margin-right: 8px;
     }
+
     .post .post-footer {
         border-top: 1px solid #ddd;
         padding: 15px;
     }
+
     .post .post-footer .input-group-addon a {
         color: #454545;
     }
+
     .post .post-footer .comments-list {
         padding: 0;
         margin-top: 20px;
         list-style-type: none;
     }
+
     .post .post-footer .comments-list .comment {
         display: block;
         width: 100%;
         margin: 20px 0;
     }
+
     .post .post-footer .comments-list .comment .avatar {
         width: 35px;
         height: 35px;
     }
+
     .post .post-footer .comments-list .comment .comment-heading {
         display: block;
         width: 100%;
     }
+
     .post .post-footer .comments-list .comment .comment-heading .user {
         font-size: 14px;
         font-weight: bold;
@@ -103,21 +124,48 @@
         margin-top: 0;
         margin-right: 10px;
     }
+
     .post .post-footer .comments-list .comment .comment-heading .time {
         font-size: 12px;
         color: #aaa;
         margin-top: 0;
         display: inline;
     }
+
     .post .post-footer .comments-list .comment .comment-body {
         margin-left: 50px;
     }
+
     .post .post-footer .comments-list .comment > .comments-list {
         margin-left: 50px;
     }
+
+    .buttons a {
+        color: #828282;
+        padding: 5px 10px 5px 10px;
+    }
+
+    .button {
+        overflow: hidden;
+        color: #454545;
+        height: 100px;
+    }
+
+    .buttons a:hover {
+        background-color: #ddd;
+        text-decoration: none;
+    }
+
+    .far {
+        padding: 5px;
+    }
+
+    .reply {
+        margin-left: 15px;
+    }
 </style>
 <div class="container bootstrap snippets bootdey">
-    <div class="col-sm-8">
+    <div class="col-sm-12">
         <div class="panel panel-white post panel-shadow">
             <div class="post-heading">
                 <div class="pull-left image">
@@ -135,13 +183,10 @@
             <div class="post-description">
                 <p>Bootdey is a gallery of free snippets resources templates and utilities for bootstrap css hmtl js
                     framework. Codes for developers and web designers</p>
-                <div class="stats">
-                    <a href="#" class="btn btn-default stat-item">
-                        <i class="fa fa-thumbs-up icon"></i>2
-                    </a>
-                    <a href="#" class="btn btn-default stat-item">
-                        <i class="fa fa-share icon"></i>12
-                    </a>
+                <div class="buttons">
+                    <span class='button'><a href="#"><i class="far fa-thumbs-up"></i>Like</a></span>
+                    <span class='button'><a href="#"><i class="far fa-comment-alt"></i>Comment</a></span>
+                    <span class='button'><a href="#"><i class="far fa-share-square"></i>Share</a></span>
                 </div>
             </div>
             <div class="post-footer">
@@ -188,6 +233,12 @@
                                     <p>Ok, cool.</p>
                                 </div>
                             </li>
+                            <a class="pull-left" href="#">
+                                <img class="avatar" src="https://bootdey.com/img/Content/user_1.jpg" alt="avatar">
+                            </a>
+                            <input class='reply' placeholder="write ur reply..." type="text" >
+
+
                         </ul>
                     </li>
                 </ul>
